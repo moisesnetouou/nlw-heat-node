94dcfd828ce26341cd3a288c08cab8a5b6091e46
moises.neto <moisesnetored@gmail.com>
2021-10-20 00:41:02 -0400

    feat: Socket.io inserido ao projeto.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import "dotenv/config";
 import express from 'express';
+import http from "http";
+import {Server} from "socket.io";
+import cors from "cors";
 
 import {router} from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  },
+});
+
+io.on("connnection", socket => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
@@ -17,4 +34,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 })
 
-app.listen(4000, ()=> console.log(`Server is running on PORT 4000`));
+serverHttp.listen(4000, ()=> console.log(`Server is running on PORT 4000`));
